0f8dd52a5ae40fbf1d7657ec188849fbd7cf2865
Encho Enevski <encho.enevski@gmail.com>
2017-06-25 21:30:12 +0300

    Add new .dll files - Phonebook.Data and Phonebook.DataBase

---
+++ Phonebook/Phonebook.DataBase/SqlConnect.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.SqlClient;
+
+namespace Phonebook.Data
+{
+    public class SqlConnect
+    {
+        private const string ConnectionString = "Data Source=.;Initial Catalog=Phonebook;Integrated Security=True";
+
+        private SqlConnection connection;
+
+        public SqlConnect()
+        {
+            this.Connection = new SqlConnection(ConnectionString);
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return this.connection;
+            }
+            private set
+            {
+                this.connection = value;
+            }
+        }
+    }
+}
